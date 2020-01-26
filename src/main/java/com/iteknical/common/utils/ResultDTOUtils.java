2be8b9efc9c5c8c6968254097b305bf6df66b068
Tony <pascalqq@126.com>
2020-01-26 15:09:03 +0800

    dev

--- src/main/java/com/iteknical/common/utils/ResultDTOUtils.java
+++ src/main/java/com/iteknical/common/utils/ResultDTOUtils.java
@@ -6,7 +6,7 @@ import com.iteknical.common.dto.ResultDTO;
  * @author Tony
  */
 public class ResultDTOUtils {
-    public <T> T checkResultAndGetData(ResultDTO<T> resultDTO) {
+    public static <T> T checkResultAndGetData(ResultDTO<T> resultDTO) {
         if (resultDTO.isSuccess() == false) {
             throw new RuntimeException("code=" + resultDTO.getCode() + ", message=" + resultDTO.getMessage());
         }
