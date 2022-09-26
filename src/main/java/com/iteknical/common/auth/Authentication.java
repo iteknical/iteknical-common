a2177c919d02c32127dbdb93e09246380db370a7
Tony <pascalqq@126.com>
2022-09-26 19:36:18 +0800

    dev

---
+++ src/main/java/com/iteknical/common/auth/Authentication.java
@@ -0,0 +1,12 @@
+package com.iteknical.common.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Authentication {
+    Class<?>[] groups() default {};
+}
